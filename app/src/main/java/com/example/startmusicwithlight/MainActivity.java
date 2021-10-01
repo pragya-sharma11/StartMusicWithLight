b105b8c01bee5ea7e26e53976be66b418923ac7e
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-01 21:47:05 +0530

    create mediaplayer for song

--- app/src/main/java/com/example/startmusicwithlight/MainActivity.java
+++ app/src/main/java/com/example/startmusicwithlight/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.startmusicwithlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
+    SensorManager sm;
+    Sensor s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this, R.raw.y);
     }
 }
